22ccc8b77ab62691b5e111fc51a19e78ad7d39b3
Sia Le Blanc <62240092+Sia-Le-Blanc@users.noreply.github.com>
2025-12-04 17:53:38 +0900

    온습도

--- hardware/arduino/room_controller.ino
+++ hardware/arduino/room_controller.ino
@@ -1,12 +1,10 @@
-// hardware/arduino/room_controller.ino 수정본 (실습 코드 핀 번호에 맞춤)
+// hardware/arduino/room_controller.ino (실습 코드 기반)
 
-#include <DHT.h>
 #include <Servo.h>
 #include <TM1637Display.h>
 
-// 핀 정의 (실습 코드와 동일하게)
+// 핀 정의
 #define DHT_PIN 2
-#define DHT_TYPE DHT11
 #define SERVO_PIN 8
 #define RGB_R 5
 #define RGB_G 6
@@ -16,16 +14,19 @@
 #define TM_DIO 10
 
 // 객체 초기화
-DHT dht(DHT_PIN, DHT_TYPE);
 Servo lightServo;
 TM1637Display display(TM_CLK, TM_DIO);
 
+// DHT11 변수
+float temperature = 0.0;
+float humidity = 0.0;
+
 void setup() {
   Serial.begin(9600);
-  dht.begin();
   lightServo.attach(SERVO_PIN);
   display.setBrightness(0x0f);
   
+  pinMode(DHT_PIN, INPUT);
   pinMode(RGB_R, OUTPUT);
   pinMode(RGB_G, OUTPUT);
   pinMode(RGB_B, OUTPUT);
@@ -51,26 +52,18 @@ void executeCommand(String cmd) {
   if (cmd.startsWith("RGB=")) {
     int r, g, b;
     sscanf(cmd.c_str(), "RGB=%d,%d,%d", &r, &g, &b);
-    
-    // 0-255 범위 제한
     r = constrain(r, 0, 255);
     g = constrain(g, 0, 255);
     b = constrain(b, 0, 255);
-    
     setRGB(r, g, b);
   }
   else if (cmd.startsWith("SERVO=")) {
     int degree = cmd.substring(6).toInt();
-    
-    // 0-180 범위 제한
     degree = constrain(degree, 0, 180);
-    
     lightServo.write(degree);
   }
   else if (cmd.startsWith("BUZZER=")) {
     int freq = cmd.substring(7).toInt();
-    
-    // 0-5000Hz 범위 제한
     if (freq > 0 && freq <= 5000) {
       tone(BUZZER_PIN, freq);
     } else {
@@ -79,31 +72,18 @@ void executeCommand(String cmd) {
   }
   else if (cmd.startsWith("TIME=")) {
     int timeValue = cmd.substring(5).toInt();
-    
-    // 0-2359 범위 제한 (00:00 ~ 23:59)
     timeValue = constrain(timeValue, 0, 2359);
-    
     display.showNumberDecEx(timeValue, 0b01000000, true);
   }
   else if (cmd == "TEMPERATURE=?") {
-    float temp = dht.readTemperature();
-    if (!isnan(temp)) {
-      Serial.print("TEMPERATURE=");
-      Serial.println(temp);
-    } else {
-      Serial.print("TEMPERATURE=");
-      Serial.println(0);
-    }
+    readDHT11();
+    Serial.print("TEMPERATURE=");
+    Serial.println(temperature);
   }
   else if (cmd == "HUMIDITY=?") {
-    float hum = dht.readHumidity();
-    if (!isnan(hum)) {
-      Serial.print("HUMIDITY=");
-      Serial.println(hum);
-    } else {
-      Serial.print("HUMIDITY=");
-      Serial.println(0);
-    }
+    readDHT11();
+    Serial.print("HUMIDITY=");
+    Serial.println(humidity);
   }
 }
 
@@ -111,4 +91,42 @@ void setRGB(int r, int g, int b) {
   analogWrite(RGB_R, r);
   analogWrite(RGB_G, g);
   analogWrite(RGB_B, b);
+}
+
+void readDHT11() {
+  uint8_t data[5] = {0, 0, 0, 0, 0};
+  uint8_t cnt = 7;
+  uint8_t idx = 0;
+
+  pinMode(DHT_PIN, OUTPUT);
+  digitalWrite(DHT_PIN, LOW);
+  delay(18);
+  digitalWrite(DHT_PIN, HIGH);
+  delayMicroseconds(40);
+  pinMode(DHT_PIN, INPUT);
+
+  unsigned int loopCnt = 10000;
+  while(digitalRead(DHT_PIN) == LOW) if(loopCnt-- == 0) return;
+  
+  loopCnt = 10000;
+  while(digitalRead(DHT_PIN) == HIGH) if(loopCnt-- == 0) return;
+
+  for (int i = 0; i < 40; i++) {
+    loopCnt = 10000;
+    while(digitalRead(DHT_PIN) == LOW) if(loopCnt-- == 0) return;
+    
+    unsigned long t = micros();
+    
+    loopCnt = 10000;
+    while(digitalRead(DHT_PIN) == HIGH) if(loopCnt-- == 0) return;
+    
+    if ((micros() - t) > 40) data[idx] |= (1 << cnt);
+    if (cnt == 0) {
+      cnt = 7;
+      idx++;
+    } else cnt--;
+  }
+
+  humidity = data[0];
+  temperature = data[2];
 }
